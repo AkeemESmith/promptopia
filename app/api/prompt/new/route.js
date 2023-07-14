58c43de09688f17f7ab5a34b9ef9eb4cf52ea0ff
akeem Smith <118077209+AkeemESmith@users.noreply.github.com>
2023-07-14 17:19:44 -0500

    prompts post to main page, search bar working

---
+++ app/api/prompt/new/route.js
@@ -0,0 +1,19 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const POST = async (req, res) => {
+  const { userId, prompt, tag } = await req.json();
+
+  try {
+    await connectToDB();
+    const newPrompt = new Prompt({
+      creator: userId,
+      prompt,
+      tag
+    })
+    await newPrompt.save();
+    return new Response(JSON.stringify(newPrompt), { status: 201 })
+  } catch (error) {
+    console.log('failed to create a new prompt', error, { status: 500 })
+  }
+}
--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -7,6 +7,9 @@ import { useRouter } from 'next/navigation';
 import Form from '@components/Form';
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
@@ -14,6 +17,27 @@ const CreatePrompt = () => {
   });
 
   const createPrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if(response.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
 
   }
 
@@ -29,3 +53,4 @@ const CreatePrompt = () => {
 }
 
 export default CreatePrompt
+
