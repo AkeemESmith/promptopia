b729e2ca36b8b2d47bff727069c1c955e8815e4f
akeem Smith <118077209+AkeemESmith@users.noreply.github.com>
2023-07-17 18:16:31 -0500

    read and write implimented

---
+++ app/profile/page.jsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useState, useEffect} from 'react'
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+  const { data: session } = useSession();
+  const { posts, setPosts} = useState('');
+
+  const fetchPosts = async () => {
+    const response = await fetch(`/api/users/${session?.user.id/posts}`);
+    const data = await response.json();
+
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    if (session?.user.id) fetchPosts();
+  }, []);
+
+
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async () => {
+
+  }
+  return (
+    <Profile
+      name="My"
+      desc="Welcome to your personalized profile page"
+      data={posts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+      
+
+  )
+}
+
+export default MyProfile;
